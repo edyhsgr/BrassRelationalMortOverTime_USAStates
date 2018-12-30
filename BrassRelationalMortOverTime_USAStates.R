be9f38ad8afe730e53cedd9537913e2d876583c6
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2018-12-29 23:19:38 -0900

    Update BrassRelationalMortOverTime_USAStates.R

--- BrassRelationalMortOverTime_USAStates.R
+++ BrassRelationalMortOverTime_USAStates.R
@@ -1,10 +1,11 @@
 ##########
 #Brass relational life table parameters by state over time review - Eddie Hunsinger, December 2018
 #
-#Following code by UToronto professor Monica Alexander as a template: https://github.com/MJAlexander/states-mortality/blob/master/lifespan_variation/lifespan.R 
+#Following code/steps by UToronto professor Monica Alexander as a template: https://github.com/MJAlexander/states-mortality/blob/master/lifespan_variation/lifespan.R 
 #(related blog post: https://www.monicaalexander.com/2018/12/21/lifespan-variation-as-a-measure-of-mortality-progress-and-inequality/) 
 #
-#All data is from the United States Mortality Database (https://usa.mortality.org/)
+#All data is from the United States Mortality Database (https://usa.mortality.org/) 
+#(downloaded/extracted the bundled lifetables.zip file)
 ##########
 
 ##########
